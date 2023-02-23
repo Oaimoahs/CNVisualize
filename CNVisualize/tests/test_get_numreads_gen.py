e0ea51e3372ac68b284fb69bca774880e135a1cf
Shaomiao Xia <shaomiao@umich.edu>
2023-02-23 00:00:06 -0500

    modified test

--- CNVisualize/tests/test_get_numreads_gen.py
+++ CNVisualize/tests/test_get_numreads_gen.py
@@ -3,6 +3,6 @@ from CNVisualize import numbywindows as m
 def test_get_numreads_gen():
     bam = "./../../data/bams/ACGCCAGAGCAGTACG-1_B12_sorted.bam"
     region = "1:500001-1000000"
-    expected_numreads = 439
+    expected_numreads = str(439)
     numreads = m.get_numreads(bam, region)
     assert numreads == expected_numreads, "The number of reads in %s of this bam is %s not %s" % (region, expected_numreads, numreads)
