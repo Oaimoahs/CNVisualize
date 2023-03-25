8acf3c6e397a0a0a054eb4106e1a2862a15a5423
Shaomiao Xia <shaomiao@umich.edu>
2023-03-25 03:49:27 -0400

    Updated code

--- CNVisualize/get_dataframe.py
+++ CNVisualize/get_dataframe.py
@@ -15,15 +15,12 @@ FORMAT = '%(levelname)s %(asctime)-15s %(name)-20s %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 logger = logging.getLogger(__name__)
 
-chromosomes = [str(i) for i in range(1, 23)] + ['X', 'Y']
-length = [249250621, 243199373, 198022430, 191154276, 180915260, 171115067, 159138663, 146364022, 141213431, 135534747, 135006516, 133851895, 115169878, 107349540, 102531392, 90354753, 81195210, 78077248, 59128983, 63025520, 48129895, 51304566, 155270560, 59373566]
-
-
 def parse_args(args):
     description = "Extract number of reads by region windows from bam files."
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("-i","--indir", required=True, type=str, help="input bam file dir")
     parser.add_argument("-b","--barcode", required=True, type=str, help="barcode list file, each line is a unique barcode")
+    parser.add_argument("-c","--chromsizes", required=True, type=str, help="chromosome size file for the reference genome, each line is a chromosome")
     parser.add_argument("-w", "--windowsize", type=int, required=False, default=500000, help="fixed window size for binning the genome")
     parser.add_argument("-o", "--outdir", type=str, required=False, default='./', help="Running directory where to write the read number csv (default: current directory)")
     args = parser.parse_args(args)
@@ -38,9 +35,18 @@ def parse_args(args):
     return {
         'bampath' : args.indir,
         'bc_file' : args.barcode,
+        'chrom_size' : args.chromsizes,
         'window_size' : args.windowsize,
         'rundir' : os.path.abspath(args.outdir)
     }
+
+def read_chrom_size(path):
+    chroms = open(path, "r")
+    chrom_size_dict = {}
+    for line in chroms.readlines():
+        line = line.strip().split("\t")
+        chrom_size_dict[line[0][3:]] = int(line[1])
+    return chrom_size_dict
 
 def get_numreads(bam, region):
     output = os.popen('samtools coverage -r %s %s' % (region, bam))
@@ -48,39 +54,36 @@ def get_numreads(bam, region):
     numreads = line.strip('\n').split('\n')[1].split('\t')[3]
     return numreads
 
-def generate_df(bam_path, out_path, barcode_list, windowsize):
-    for bc in barcode_list:
-        bam_file = os.path.join(bam_path, bc + "_sorted.bam")
-        reads = []
-        logger.info('Start %s' % bc)
-        for chr in chromosomes:
-            logger.info('Start %s' % chr)
-            reads.append('chr'+chr)
-            len = length[chromosomes.index(chr)]
-            for i in range(math.ceil(len/windowsize)):
-                start = i * windowsize + 1
-                end = (i + 1) * windowsize
-                if end > len:
-                    end = len
-                reg = chr + ':' + str(start) + '-' + str(end)
-                numreads = get_numreads(bam_file, reg)
-                reads.append(numreads)
+def window_indices(size, chrom_sizes):
+    chroms = list(chrom_sizes.keys())
+    windows_region = []
+    for chrom in chroms:
+        pos = 0
+        while pos + 1 + size < chrom_sizes[chrom]:
+            windows_region.append(chrom + ":" + str(pos+1) + "-" + str(pos+size))
+            pos += size
+        windows_region.append(chrom + ":" + str(pos+1) + "-" + str(chrom_sizes[chrom]))
+    return windows_region
+
+def generate_df(bc_path, bam_path, regions, out_path):
+    for cell in open("data/sample_barcodes.txt", "r").readlines():
+        count = []
+        path = os.path.join(bam_path, cell.strip() + "_sorted.bam")
+        for region in regions:
+            numreads = get_numreads(path, region)
+            count.append(numreads)
         with open(out_path, 'a+') as f:
             csv_write = csv.writer(f)
-            data_row = [bc] + reads
+            data_row = [cell.strip()] + count
             csv_write.writerow(data_row)
 
 def main(args=None):
     args = parse_args(args)
     logger.info('\n'.join(['Arguments:'] + [f'{a} : {args[a]}' for a in args]))
     
-    bc_list = []
-    with open(args["bc_file"]) as fin:
-        for line in fin:
-            barcode = line.strip()
-            bc_list.append(barcode)
-
-    generate_df(args["bampath"], args["rundir"], bc_list, args["window_size"])
+    chrom_size_dict = read_chrom_size(args["chrom_size"])
+    windows_region = window_indices(args["window_size"], chrom_size_dict)
+    generate_df(args["bc_file"], args["bampath"], windows_region, args["rundir"])
 
 if __name__ == "__main__":
     main()
